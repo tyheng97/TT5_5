46e637ff64c5aee936f71a3399708c24751d7175
cht <1901880@sit.singaporetech.edu.sg>
2022-05-06 11:44:04 +0800

    fixed errors in project js

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -5,13 +5,11 @@ const ProjectSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      unique: false,
     },
 
     description: {
       type: String,
       required:true,
-      required: false,
     },
     budget: {
       type:float,
